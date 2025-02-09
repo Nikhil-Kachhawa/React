486c0fc6e1b772a8c5a8aa85f746f00074e2ff80
Nikhil-Kachhawa <nikhilkachhawa31@gmail.com>
2025-02-09 03:14:02 +0100

    React useEffect Hook

--- 7.useEffect Hook/src/App.jsx
+++ 7.useEffect Hook/src/App.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const App = () => {
+
+  const [data, setData] = useState([])
+  useEffect(
+    () => {
+      async function getData () {
+        const response  = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json()
+        setData(data)
+        // console.log(data)
+      }
+      getData()  
+    }
+    
+  , [])
+
   return (
-    <div>App</div>
+    <div>
+
+    <h1>: Random Users of the Day :</h1>
+    <ul>
+      {
+        data.map(
+          (item) => (
+            <div key={item.id}>
+              <h2>Name : {item.name}</h2>
+              <h3>Username : {item.username}</h3>
+              <h3>Email : {item.email}</h3>
+              <br />
+            </div>
+          )
+        )
+      }
+    </ul>  
+    </div>
   )
 }
 
