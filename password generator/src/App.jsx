efce221807242df943374fb78d03e92efd527868
Nikhil-Kachhawa <nikhilkachhawa31@gmail.com>
2025-03-09 21:27:23 +0100

    Working on random password logic

--- password generator/src/App.jsx
+++ password generator/src/App.jsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 
 const App = () => {
+
+  const [rangeValue, setRangeValue] = useState(4)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [numAllowed, setNumAllowed] = useState(false)
+  const [pwd, setPwd] = useState('')
+
+  const password = useCallback( () => {
+    let text = ""
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    const possible2 = "0123456789"
+    const possible3 = "!@#$%^&*()_+"
+
+    
+  }
+  , [rangeValue, charAllowed, numAllowed, setPwd])
+
+  const handleClick = () => {
+    console.log("ads");
+  }
+
+  const handleRangeChange = (e) => {
+    setRangeValue(e.target.value)
+    setPwd(pwd+text)
+    
+  }
+
   return (
-    <div>App</div>
+    // <div className = "flex w-screen h-screen border-4 justify-center border-indigo-500">
+    <div className = "border-indigo-500">
+        <h1 className="text-4xl border mt-4">: Password Generator :</h1>
+        
+        <input type="text" name="pwd" id="pwd" 
+          className="bg-green-100 text-black font-bold" 
+          value={pwd}
+        />
+        
+        <button 
+          className="bg-indigo-500 hover:bg-green-700 text-white font-bold" 
+          onClick={handleClick}
+        > Copy ! </button>
+        
+        <br />
+
+        <label htmlFor="range1to100">
+          <input type="range" name="range4to24" id="range4to24" 
+            min="4" 
+            max="24" 
+            onChange={handleRangeChange} 
+            value={rangeValue} 
+          />
+          {rangeValue}
+        </label>
+
+        <br />
+
+        <label htmlFor="charAllowed">
+          <input type="checkbox" name="charAllowed" id="charAllowed"
+            onClick={() => pass}
+          />
+          Characters Allowed ? 
+        </label>
+        
+        <br />
+
+        <label htmlFor="numAllowed">
+          <input type="checkbox" name="numAllowed" id="numAllowed" 
+            onClick={() => pass}
+          />
+          Numbers Allowed ? 
+        </label>
+    
+    </div>
   )
 }
 
